8cc7ad458dcefd0f178d62f70ff78721a5426e45
hai <haidb.1280@gmail.com>
2024-09-22 15:58:30 +0700

    fix start pr

--- client/routers/users-route.js
+++ client/routers/users-route.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const userController = require('../controllers/user-controller')
 
 router.post('/login',userController.login)
+router.post('/register',userController.registerUser)
+
 router.use('/', (req, res) => {
   return res.status(404).json(new ResponseData(false, "Not found").toJson())
 })
